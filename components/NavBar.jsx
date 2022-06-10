bf9163d5af42f1909c379bd9c9fffae561afd5df
TerminatorxxD <tuanlinhdoan2@gmail.com>
2022-06-10 10:24:35 +0700

    9nd commit

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -3,12 +3,12 @@ import Link from 'next/link';
 import {AiOutlineShopping} from 'react-icons/ai';
 import Cart from './Cart';
 import { useStateContext } from '../context/StateContext';
-// import {useUser} from '@auth0/nextjs-auth0';
+import {useUser} from '@auth0/nextjs-auth0';
 
  
 const NavBar = () => {
   
-  // const {user} = useUser();
+  const {user} = useUser();
   const {showCart, setShowCart, totalQuantities} = useStateContext();
 
   return (
@@ -21,10 +21,12 @@ const NavBar = () => {
         <span className="cart-item-qty">{totalQuantities}</span> 
       </button>
       {showCart && <Cart />}
-      <Link href="/api/auth/login">
+      {!user && (
+        <Link href="/api/auth/login">
         <button type="button" className="auth-button">Login</button>
-      </Link>
-      {/* {user && (
+        </Link>
+      )}
+      {user && (
         <div>
           <Link href="/api/auth/logout">
             <button type="button" className="auth-button">Logout</button>
@@ -33,7 +35,7 @@ const NavBar = () => {
             <button type="button" className="auth-button">Admin</button>
           </Link>
         </div>
-      )} */}
+      )}
 
       
     </div>
